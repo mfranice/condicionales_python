1f453ebb127d5d89de3b4eeec9c97b60f6e689fa
mfranice <mfranice@gmail.com>
2021-05-03 22:54:07 -0300

    Ejercicios de práctica 1 y 2 terminados

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -19,12 +19,35 @@ numero_2 = int(input('Ingrese el segundo número:\n'))
 # Compare cual de los dos números es mayor
 # Imprima en pantalla según corresponda
 
+if numero_1 > numero_2:
+    print('El primer número ingresado ({}) es mayor que el segundo ({})'.format(numero_1,numero_2))
+elif numero_1 < numero_2:
+    print('El primer número ingresado ({}) es menor que el segundo({})'.format(numero_1,numero_2))
+else: 
+    print('Ambos números ingresados ({} y {})son iguales'.format(numero_1,numero_2))
+
 # Verifique si el numero_1 positivo, negativo o cero
 # Imprima el resultado en cada caso
+
+if numero_1>0:
+    print('El primer número ingresado ({}) es positivo.'.format(numero_1,numero_2))
+elif numero_1<0:
+    print('El primer número ingresado ({}) es negativo.'.format(numero_1,numero_2))
+else:
+    print('El primer número ingresado ({}) es cero.'.format(numero_1,numero_2))
 
 # Verifique si el numero_1 es mayor a 0 y menor a 100
 # Imprima en pantalla si se cumple o no la condición
 
+if numero_1>0 and numero_1<100:
+    print('El primer número ingresado ({}) cumple con la condición.'.format(numero_1,numero_2))
+else:
+    print('El primer número ingresado ({}) no cumple con la condición.'.format(numero_1,numero_2))
+
 # Verifique si el numero_1 es menor a 10 o el numero_2
 # es mayor a -2
 # Imprima en pantalla si se cumple o no la condición
+if numero_1<10 or numero_2>-2:
+    print('Los numeros ingresados ({} y {}) cumplen con la condición.'.format(numero_1,numero_2))
+else:
+    print('Los numeros ingresados ({} y {}) no cumplen con la condición.'.format(numero_1,numero_2))
--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,18 +19,43 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print('La primera palabra ingresada ({}) es mayor (alfabeticamente) que la segunda ({})'.format(texto_1,texto_2))
+elif texto_1 < texto_2:
+    print('La segunda palabra ingresada ({}) es mayor (alfabeticamente) que la primera ({})'.format(texto_2,texto_1)) 
+else:
+    print('Ambas palabras ingresadas ({}) son iguales'.format(texto_1,texto_2))
+
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
 # Imprima en pantalla según corresponda
+
+if len(texto_1) > len(texto_2):
+    print('La primera palabra ingresada ({}) es más larga que la segunda ({})'.format(texto_1,texto_2))
+elif len(texto_1) < len(texto_2):
+    print('La segunda palabra ingresada ({}) es más larga que la primera ({})'.format(texto_2,texto_1)) 
+else:
+    print('Ambas palabras ingresadas ({} y {}) tienen la misma cantidad de letras'.format(texto_1,texto_2))
 
 # Verifique si la primera letra de la primera palabra
 # es mayor a la primera letra de la segunda palabra
 # Imprima en pantalla según corresponda
 
+if texto_1 [0] > texto_2[0]:
+    print('La primera letra de la primera palabra ingresada ({}) es mayor que la de la segunda ({})'.format(texto_1,texto_2))
+elif texto_1 [0] < texto_2 [0]:
+    print('La primera letra de la segunda palabra ingresada ({}) es mayor que la de la primera ({})'.format(texto_2,texto_1)) 
+else:
+    print('Las primeras letras de ambas palabras ingresadas ({} y {}) son iguales'.format(texto_1,texto_2))
+
+
 copia_texto_1 = texto_1  # Copia de la variable texto_1
 
 # Verifique que copia_texto_1 es igual a texto_1
 # Imprima en pantalla según corresponda
-
+if texto_1 == copia_texto_1:
+    print('La variable copia_texto_1 ({}) es igual a la variable texto_1 ({})'.format(copia_texto_1,texto_1))
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+if texto_1 != copia_texto_1:
+    print('La variable copia_texto_1 ({}) es igual a la variable texto_1 ({})'.format(copia_texto_1,texto_1))
